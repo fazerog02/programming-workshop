06e88819392889996fa454687a411ffd39fe9f0b
fazerog02 <39889014+fazerog02@users.noreply.github.com>
2022-04-16 14:29:43 +0900

    [add]四則演算用のテストコード

---
+++ calc.py
@@ -0,0 +1,17 @@
+print("1 + 14 = ", end="")
+print(1 + 14)
+
+print("5 - 1 = ", end="")
+print(5 - 1)
+
+print("3 * 2 = ", end="")
+print(3 * 2)
+
+print("10 / 2 = ", end="")
+print(10 / 2)
+
+print("1 + 2 * 3 = ", end="")
+print(1 + 2 * 3)
+
+print("(1 + 2) * 3 = ", end="")
+print((1 + 2) * 3)
