44e5e7ac68bd77287e60bd8dd0e8d13910ad9b92
fazerog02 <39889014+fazerog02@users.noreply.github.com>
2022-04-19 14:07:50 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,17 +1,11 @@
-print("1 + 14 = ", end="")
-print(1 + 14)
+print("1 + 14 = ", 1 + 14)
 
-print("5 - 1 = ", end="")
-print(5 - 1)
+print("5 - 1 = ", 5 - 1)
 
-print("3 * 2 = ", end="")
-print(3 * 2)
+print("3 * 2 = ", 3 * 2)
 
-print("10 / 2 = ", end="")
-print(10 / 2)
+print("10 / 2 = ", 10 / 2)
 
-print("1 + 2 * 3 = ", end="")
-print(1 + 2 * 3)
+print("1 + 2 * 3 = ", 1 + 2 * 3)
 
-print("(1 + 2) * 3 = ", end="")
-print((1 + 2) * 3)
+print("(1 + 2) * 3 = ", (1 + 2) * 3)
